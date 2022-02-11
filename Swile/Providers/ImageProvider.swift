f73a9a1e27f7cccf91805bb7352a88f246a02f31
tfromont <tho.fromont@gmail.com>
2022-02-11 12:08:51 +0100

    [ImageProvider] add local cache

--- Swile/Providers/ImageProvider.swift
+++ Swile/Providers/ImageProvider.swift
@@ -13,6 +13,10 @@ protocol ImageProviderType: AnyObject {
 }
 
 class ImageProvider: ImageProviderType {
+
+    private var cachedImages: [String: UIImage] = [:]
+    private let queue = DispatchQueue(label: "swile.imageprovider", qos: DispatchQoS.userInteractive)
+
     func rx_image(from url: String?, defaultImage: UIImage?) -> Observable<UIImage?> {
         guard let urlString = url, let url = URL(string: urlString) else {
             return .just(defaultImage)
@@ -20,9 +24,24 @@ class ImageProvider: ImageProviderType {
 
         let request = URLRequest(url: url)
 
+        var startImage = defaultImage
+        queue.sync {
+            if let cachedImage = cachedImages[urlString] {
+                startImage = cachedImage
+            }
+        }
+
         return URLSession.shared.rx
             .response(request: request)
             .map { UIImage(data: $0.1) }
-            .startWith(defaultImage)
+            .do { [weak self] image in
+                guard let self = self else {
+                    return
+                }
+                self.queue.sync {
+                    self.cachedImages[urlString] = image
+                }
+            }
+            .startWith(startImage)
     }
 }
